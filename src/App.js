8153b69c265f59b7e8e312446311911bfbbbedec
ivanov9826 <ivanov9826@gmail.com>
2022-08-07 16:43:37 +0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
+import {Routes , Route} from 'react-router-dom';
+
 function App() {
     return (
         <div id="box">
             <Header />
             {/* Main Content */}
             <main id="main-content">
-                <Home />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+                
             </main>
             {/*Home Page*/}
 
